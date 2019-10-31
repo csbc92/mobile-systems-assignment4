f2331387f8abf15de2805c2e7358b8be53306862
Christian Clausen <csbc92@hotmail.com>
2019-10-31 18:56:48 +0100

    Updated the performance test to create an output report

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-artillery run performance-test.yml
+
+# Output the JSON result (raw data) to out/result.json
+# and create an HTML report based on the raw file. 
+artillery run performance-test.yml -o out/result.json
+artillery report -o out/report.html out/result.json
